46149bd1f94d5e269db12e8931745fd075391275
hhefesto <hhefesto@rdataa.com>
2021-12-19 20:44:52 -0600

    servant file upload successful

---
+++ app/Main.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
+
+import           Control.Concurrent
+import           Control.Exception
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Data.Text.Encoding                    (encodeUtf8)
+import           Network.HTTP.Client                   hiding (Proxy)
+import           Network.HTTP.Client.MultipartFormData
+import           Network.Socket                        (withSocketsDo)
+import           Network.Wai.Handler.Warp
+import           Servant
+import           Servant.Multipart
+
+import qualified Data.ByteString.Lazy                  as LBS
+
+-- https://docs.servant.dev/en/stable/cookbook/file-upload/FileUpload.html
+
+type API = MultipartForm Mem (MultipartData Mem) :> Post '[JSON] Integer
+
+api :: Proxy API
+api = Proxy
+
+-- MultipartData consists in textual inputs,
+-- accessible through its "inputs" field, as well
+-- as files, accessible through its "files" field.
+upload :: Server API
+upload multipartData = do
+  liftIO $ do
+    putStrLn "Inputs:"
+    forM_ (inputs multipartData) $ \input ->
+      putStrLn $ "  " ++ show (iName input)
+            ++ " -> " ++ show (iValue input)
+
+    forM_ (files multipartData) $ \file -> do
+      let content = fdPayload file
+      putStrLn $ "Content of " ++ show (fdFileName file)
+      LBS.putStr content
+  return 0
+
+startServer :: IO ()
+startServer = run 8080 (serve api upload)
+
+main :: IO ()
+main = withSocketsDo . bracket (forkIO startServer) killThread $ \_threadid -> do
+  -- we fork the server in a separate thread and send a test
+  -- request to it from the main thread.
+  manager <- newManager defaultManagerSettings
+  req <- parseRequest "http://localhost:8080/"
+  resp <- flip httpLbs manager =<< formDataBody form req
+  print resp
+
+  where form =
+          [ partBS "title" "World"
+          , partBS "text" $ encodeUtf8 "Hello"
+          , partFileSource "file" "./README.md"
+          ]
---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Nix flake for servant backend of playNFT";
+
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix, flake-compat }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    let
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          # This overlay adds our project to pkgs
+          servant-backend-playnft = final.haskell-nix.cabalProject {
+            # If these null parameters are absent, you get a RestrictedPathError error
+            # from trying to do readIfExists on cabal.project file
+            cabalProjectFreeze = null;
+            cabalProject = null;
+            cabalProjectLocal = null;
+
+            src = final.haskell-nix.cleanSourceHaskell {
+              src = ./.;
+              name = "servant-backend-playnft";
+            };
+            # compiler-nix-name = "ghc884";
+            compiler-nix-name = "ghc8107";
+            # compiler-nix-name = "ghc921";
+            # This is used by `nix develop .` to open a shell for use with
+            # `cabal`, `hlint` and `haskell-language-server`
+            shell.tools = {
+              cabal = {};
+              haskell-language-server = {};
+            };
+          };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      flake = pkgs.servant-backend-playnft.flake {};
+    in flake // {
+      # Built by `nix build .`
+      defaultPackage = flake.packages."servant-backend-playnft:exe:servant-backend-playnft";
+
+      # This is used by `nix develop .` to open a shell for use with
+      # `cabal`, `hlint` and `haskell-language-server`
+    });
+}
